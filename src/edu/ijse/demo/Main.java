d78614a59bbf80c4e1af9609016f779a8ef117a3
Kavindusasmitha <Kavindusasmitha20@gmail.com>
2025-03-26 11:52:34 +0530

    DB and DTO

--- src/edu/ijse/demo/Main.java
+++ src/edu/ijse/demo/Main.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package edu.ijse.demo.Main;
+package edu.ijse.demo;
 
 /**
  *
  * @author kavindu-sasmitha
  */
-public class java {
+public class Main {
 
     /**
      * @param args the command line arguments
