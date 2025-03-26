ba4a43b2223edc5593216153fb620956cbdfcdaa
Kavindusasmitha <Kavindusasmitha20@gmail.com>
2025-03-26 09:47:33 +0530

    my first commit

---
+++ src/edu/ijse/demo/Main/java.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package edu.ijse.demo.Main;
+
+/**
+ *
+ * @author kavindu-sasmitha
+ */
+public class java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        // TODO code application logic here
+    }
+    
+}
