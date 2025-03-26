25b980a5b03af32a2cfbe4dd32d37cddc81b7c60
Kavindusasmitha <Kavindusasmitha20@gmail.com>
2025-03-26 13:10:52 +0530

    DB and DTO

---
+++ src/edu/ijse/demo/model/ItemModel.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ijse.demo.model;
+
+/**
+ *
+ * @author kavindu-sasmitha
+ */
+public class ItemModel {
+    
+}
